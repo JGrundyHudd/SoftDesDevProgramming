dbb13fdd767d8ae177b8c0ee759d4c6a0e28a7fa
JGrundyHudd <u1757909@unimail.hud.ac.uk>
2017-10-19 12:36:15 +0100

    added pupilteammaker

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 user_name = input('Please enter your name: ')
 if len(user_name) > 0:
-    print('Hello ' + user_name + ', how pleasant it is to meet you.')
+    print('Hello ' + user_name.capitalize() + ', how pleasant it is to meet you.')
 else:
     print('Hello World')
