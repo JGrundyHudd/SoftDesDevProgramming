e9ac1d8640f6858ce77eb5b9b5964c72dbf6d9d3
JGrundyHudd <U1757909@unimail.hud.ac.uk>
2017-12-13 14:41:13 +0000

    updated TechHelpline

--- TechHelpline.py
+++ TechHelpline.py
@@ -12,15 +12,27 @@ def return_answer(question):
         "Backup your data and do a factory reset."
     elif "Game" in question:
         "Update your graphics drivers"
+    elif "Vista" in question:
+        "Just let it die in peace, it's for the best"
     else:
-        return "GIT"
+        random_choice = random.randint(0, 5)
+        if random_choice == 0:
+            return "Are you sure it's on?"
+        elif random_choice == 1:
+            return "Try rerouting main power through the secondary coupling"
+        elif random_choice == 2:
+            return "Have you tried forcing an unexpected reboot?"
+        elif random_choice == 3:
+            return "Yeah that does sound pretty bad. Oh well."
+        elif random_choice == 4:
+            return "Sounds as if you need our new guide to troubleshooting, just £34.99 this week only"
 
 
 def random_disconnect():
     return random.randint(1, 11)
 
 
-input("Please enter your name: ")
+input("Hello, what is your name? ")
 while 1:
     question_input = input("Please enter your question: ")
     if random_disconnect() == 1:
@@ -30,4 +42,3 @@ while 1:
         break
     else:
         print(return_answer(question_input))
-
