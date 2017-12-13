69c6094118161b5025edf3017128705b1866ea4b
JGrundyHudd <U1757909@unimail.hud.ac.uk>
2017-12-13 13:03:55 +0000

    updated TechHelpline

--- TechHelpline.py
+++ TechHelpline.py
@@ -3,20 +3,31 @@ import random
 
 def return_answer(question):
     if "Windows" in question:
-        return "Brimble my doobletip"
+        return "Have you tried turning it off and on again?"
     elif "Apple" in question:
-        "please kill yourself"
+        "Have you considered upgrading to the latest offering?"
+    elif "Linux" in question:
+        "What? You're on your own mate."
+    elif "Android" in question:
+        "Backup your data and do a factory reset."
+    elif "Game" in question:
+        "Update your graphics drivers"
     else:
         return "GIT"
 
 
 def random_disconnect():
-    if random.randint(1,11) == 1:
-        return 1
+    return random.randint(1, 11)
 
 
 input("Please enter your name: ")
 while 1:
-    print(return_answer(input("Please enter your question: ")))
+    question_input = input("Please enter your question: ")
     if random_disconnect() == 1:
         break
+    if question_input.capitalize() == "Goodbye":
+        print("Goodbye")
+        break
+    else:
+        print(return_answer(question_input))
+
